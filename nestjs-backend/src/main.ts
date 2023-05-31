91ba60471c42f81b5b76d2fa24b8c3a8b401f3a2
Florian DA <florian.almeida05@gmail.com>
2023-06-01 01:22:57 +0200

    Try fix "Mixed Content" error

--- nestjs-backend/src/main.ts
+++ nestjs-backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('../secret/routementor_key.pem'),
+    cert: fs.readFileSync('../secret/routementor_cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(3000);
 }
 bootstrap();
